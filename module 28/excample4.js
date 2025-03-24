499a76ca36696fcfbde0bf618e0fcd6167abff60
eros-DEV21 <eroskabashi97@gmail.com>
2025-03-24 19:48:53 +0100

    push

---
+++ module 28/excample4.js
@@ -0,0 +1,12 @@
+var button2 =document.getElementById('btn2');
+var button3 =document.getElementById('btn3');
+var button4 =document.getElementById('btn4');
+button2.onmouseover = function(){
+    alert('hello');
+}
+button3.onmouseleave = function(){
+    alert('hello');
+}
+button4.onmousewheel = function(){
+    alert('hello');
+}
