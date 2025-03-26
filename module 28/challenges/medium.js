c8e506b34c5555baa6b2628146198b4fb808d680
ledisopa <ledisopa1@gmail.com>
2025-03-26 19:27:32 +0100

    module28 medium

--- module 28/challenges/medium.js
+++ module 28/challenges/medium.js
@@ -1,5 +1,7 @@
-var shkolla={
-    emri:"vellezrit frashri",
-    klasa:"IX",
-    paralelja:"I"
-}
+btn1.onclick=function () {
+    txt1.style.color="red";
+    txt1.style.backgroundColor="lightgray";
+    txt1.style.textAlign="center";
+    txt1.style.fontsize="100px";
+    txt1.style.padding="20px";
+    }
