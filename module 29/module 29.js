b1113d73172b1a5d53d4f93e7dd121f77eeb5759
eros-DEV21 <eroskabashi97@gmail.com>
2025-04-02 19:11:24 +0200

    module 29

--- module 29/module 29.js
+++ module 29/module 29.js
@@ -51,3 +51,25 @@ console.log(s1);
 
 
 console.log(s2);
+
+
+var places =["London","Paris","New York","Berlin"]
+
+var[FirstPlace, ,secondPlace] = places
+
+console.log(secondPlace)
+
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+
+var[first,second, ...otherNumbers]= numbers;
+
+
+console.log(first);
+
+
+console.log(second);
+
+
+console.log(otherNumbers.toString());
