a6eb7b062eaf796c6cad480ce70500245fc1e0ce
eros-DEV21 <eroskabashi97@gmail.com>
2025-04-14 19:15:22 +0200

    module 30

--- module 29/module 29 east challange.js
+++ module 29/module 29 east challange.js
@@ -1,4 +1,4 @@
-var names=['eros','gent','ledi'];
+var names=['eros 1','gent 2','ledi 3'];
 
 
 console.log(names[1]);
---
+++ module 30/main.js
@@ -0,0 +1,33 @@
+var input = document.getElementById('input_id');
+var button = document.getElementById('btn_id');
+var text = document.getElementById('text_id');
+
+//button.onclick = function(){
+  //text.innerHTML = input.value;
+//}
+
+
+var input1 = document.querySelector('#input1_id');
+var input2 = document.querySelector('#input2_id');
+var btn2 = document.querySelector('#btn2_id');
+var ans = document.querySelector('#answer');
+
+btn2.addEventListener("click",function(){
+    ans.innerHTML = parseInt(input1.value) + parseInt(input2.value);
+})
+
+
+button.onclick = function (){
+    if(input.value > 10)
+    {
+        text.innerHTML="input value is greater than 10";
+    }
+    else if (input.value < 10)
+    {
+        text.innerHTML="input value is less than 10";
+    }
+    else
+    {
+        text.innerHTML="input value is equal than 10"; 
+    }
+}
