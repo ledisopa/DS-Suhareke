48b41757771e5d5bc01eeac584f66972af1e2eb2
ledisopa <ledisopa1@gmail.com>
2025-04-16 19:20:08 +0200

    module 30a

---
+++ module 30a/main.js
@@ -0,0 +1,13 @@
+var wakeuptime;
+var dstime;
+var sleeptime;
+var noon = 12;
+
+
+function showCurrentTime(){
+    var clock = document.getElementById('clock');
+    var currentTime = new Date();
+
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes;
+}
