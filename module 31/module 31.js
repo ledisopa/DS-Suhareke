3bc7d5c2b9baf39c8ced053b5a4243565983f49a
eros-DEV21 <eroskabashi97@gmail.com>
2025-04-23 19:19:16 +0200

    module 31 loops

---
+++ module 31/module 31.js
@@ -0,0 +1,34 @@
+//do while loop
+
+var i=0;
+do{
+    i = i+1;
+    console.log(i);
+}while(i<5);
+
+console.log("------------------")
+//while loop
+var j=0;
+while(j<5){
+    j=j+1;
+    console.log(j)
+}
+console.log("----------------------------")
+
+
+let n=0;
+let x=0;
+
+while(n<3){
+    n++;
+    x+=n;
+    console.log(n);
+    console.log(x);
+}
+console.log("----------------------------")
+//for loop
+
+for(var i=0; i<5; i++){
+    console.log("value of i is: "+i);
+}
+console.log("------------------------")
