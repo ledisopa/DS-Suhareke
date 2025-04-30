bad826117db71e62c50ee2af2189b2c77389c4cb
ledisopa <ledisopa1@gmail.com>
2025-04-30 19:18:12 +0200

    module 32

---
+++ module 32/book-examples/main.js
@@ -0,0 +1,49 @@
+/*var input =document.getElementById('input_id');
+var button =document.getElementById('btn_id');
+var text =document.getElementById('teksti_id');
+
+button.onclick=function(){
+    text.innerHTML=input.value;
+}*/
+
+/*var text="The best school in th world is Digital School";
+var result=text.search("Digital School");
+document.getElementById("result1").innerHTML = result;*/
+
+
+/*var text="The best school in th world is Digital School";
+var result=text.search(/Digital School/);
+document.getElementById("result2").innerHTML = result;*/
+
+
+/*var text="The best school in th world is Digital School";
+var result=text.search(/Digital School/,"Another School");
+document.getElementById("result3").innerHTML = result;*/
+
+
+/*var text="abcdef";
+var regex=new RegExp('abc');
+document.getElementById("result4").innerHTML =regex.test(text);*/
+
+
+
+/*var text="My school the best school in the world";
+var regex=/school/g;
+document.getElementById("result5").innerHTML =text.match(regex);*/
+
+
+
+/*var text="Digital School the best school in the world";
+var regex=/i/g;
+document.getElementById("result6").innerHTML=text.match(regex);*/
+
+/*var text="Digital School the best school in the world";
+var regex=/[abc]/g;
+document.getElementById("result7").innerHTML=text.match(regex);*/
+
+
+var text="Digital School is in top 10 best schools of the world";
+var regex=/[0-9]/g;
+document.getElementById("result8").innerHTML=text.match(regex)
+
+
