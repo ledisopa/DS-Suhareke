db6b078ba21f45d35b9146106579a905343a3f92
eros-DEV21 <eroskabashi97@gmail.com>
2025-05-05 18:51:18 +0200

    module32

--- module 32/main.js
+++ module 32/main.js
@@ -30,12 +30,51 @@ document.getElementById("result5").innerHTML=text.match(regex)*/
 var regex=/i/g;
 document.getElementById("result6").innerHTML=text.match(regex)*/
 
-var text= "Digital school is the best school in the world";
+/*var text= "Digital school is the best school in the world";
 var regex=/[abc]/g;
-document.getElementById("result7").innerHTML=text.match(regex)
+document.getElementById("result7").innerHTML=text.match(regex)*/
 
-var text= "Digital school is in top 10 best schools in the world!";
+/*var text= "Digital school is in top 10 best schools in the world!";
 var regex=/[0-9]/g;
-document.getElementById("result8").innerHTML=text.match(regex)
+document.getElementById("result8").innerHTML=text.match(regex);*/
 
+//var text="My school is th best school in the world!";
+//var regex=/(top|best|school)/g;
+//document.getElementById("result9").innerHTML=text.match(regex);
+
+//var text= "100 percent!";
+//var regex=/\d/g;
+//document.getElementById("result10").innerHTML=text.match(regex);
+
+//var text= "My school is the beast school in the world!";
+//var regex=/\s/g;
+//document.getElementById("result11").innerHTML=text.match(regex);
+
+//var text= "Heeey,how are you!";
+//var regex=/e /g;
+//document.getElementById("result12").innerHTML=text.match(regex)
+
+//var text= "so, I hope we will see each other soon again...";
+//var regex=/so*/g;
+//document.getElementById("result13").innerHTML=text.match(regex);
+
+//var text= "hey,hi,hiii!";
+//var regex=/h1?/g;
+//document.getElementById("result14").innerHTML=text.match(regex);
+
+//var text= "hey,hi,hiii!";
+//var regex=/h1?/g;
+//document.getElementById("result15").innerHTML=text.match(regex);
+
+//var text= "hello,hellooo, hellooooooo!";
+//var regex=/o{3,5/g;
+//document.getElementById("result16").innerHTML=text.match(regex);
+
+//var text= "bestfriend, boyfriend, girlfriend!";
+//var regex=/end$/g;
+//document.getElementById("result17").innerHTML=text.match(regex)
+
+//var text= "cat, catalog, category";
+//var regex=/^cat/g;
+//document.getElementById("result18").innerHTML=text.match(regex)
 
