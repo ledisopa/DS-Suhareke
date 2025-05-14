3f11c38dabe37f70d1096904742936ee30c056f9
jo1-8899-2x <jongashi010@gmail.com>
2025-05-14 18:39:48 +0200

    push

---
+++ module 34/main1.js
@@ -0,0 +1,30 @@
+var circle=document.getElementById("circle");
+var showTime=document.getElementById("time");
+var timestart=new Date().getTime();
+
+function showCircle()
+{
+    circle.style.display='block';
+    circle.style.top = Math.random()*500 + 'px';
+    circle.style.top = left.random()*900 + 'px';
+    circle.style.top = backgroundcolor=randomColor();
+    timestart=new Date().getTime();
+
+}
+
+showCircle();
+
+circle.onclick = function (){
+    circle.style.display='none';
+    setTimeout(showCircle,1000);
+    var timeEnd =new Date().getTime();
+    showTime.innerHTML = time+'s'; 
+}
+function randomColor(){
+    var s='0123456789ABCDEF';
+    var color='#';
+    for(var i = 0; i<6;i++){
+        color=color + s[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
