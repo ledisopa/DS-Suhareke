ffc10c35ff3235976e6db20ba096ace80dd226a3
ledisopa <ledisopa1@gmail.com>
2025-06-16 18:14:24 +0200

    modul 30

--- module 30a/main.js
+++ module 30a/main.js
@@ -10,4 +10,24 @@ function showCurrentTime(){
 
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes;
+    var seconds = currentTime.getSeconds;
+
+    var meridian = "AM";
+    if(hours >= noon){
+        meridian = "PM";
+    }
+
+
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " meridian;
+    clock.innerText = clockTime;
+    changeImage();
+
+}
+
+var oneSecond = 1000;
+
+setInterval(showCurrentTime, oneSecond);
+
+function changeImage(){
+    var time = new
 }
